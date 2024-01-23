bf56d9cde3660bd6666977401c57eaf3ac8586b0
phipham0 <90461213+phipham0@users.noreply.github.com>
2024-01-22 21:16:53 -0600

    added header and teampass buttons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import Header from './components/Header';
+import TeamOrPass from './components/TeamOrPass';
+
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -10,8 +13,12 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-center align-center min-100-vh bg-primary">
-        <Outlet />
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <TeamOrPass />
+        <div className="flex-column justify-center align-center min-100-vh bg-primary">
+          <Outlet />
+        </div>
       </div>
     </ApolloProvider>
   );
