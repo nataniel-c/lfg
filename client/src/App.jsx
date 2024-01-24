4c0efd123578691b15875fce9399fdb06f321452
Phi Pham <90461213+phipham0@users.noreply.github.com>
2024-01-23 21:16:24 -0600

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,3 @@
-// import './App.css';
-// import { Outlet } from 'react-router-dom';
-// import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-// import PlayerInfoCard from './components/PlayerInfoCard';
-// import Header from './components/Header';
-// import TeamOrPass from './components/TeamOrPass';
-
-// import FriendList from './components/FriendList';
-
-// const client = new ApolloClient({
-//   uri: '/graphql',
-//   cache: new InMemoryCache(),
-// });
-
-// function App() {
-//   return (
-//     <ApolloProvider client={client}>
-//       <PlayerInfoCard user={user}/>
-//       <div className="flex-column justify-flex-start min-100-vh">
-//         <Header />
-//         <TeamOrPass />
-//         <FriendList />
-//         <div className="flex-column justify-center align-center min-100-vh bg-primary">
-//           <Outlet />
-//         </div>
-//       </div>
-//     </ApolloProvider>
-//   );
-// }
-
-// export default App;
-
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
