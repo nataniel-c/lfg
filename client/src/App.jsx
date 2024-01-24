830bee02d3f4a6fef3badfaa415993c3d4824a26
Nightfeller <family2earls@gmail.com>
2024-01-23 21:09:29 -0600

    created sidebar for FriendList and modified App.jsx to work without server-side. Also changed the website name in index.html

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,46 @@
+// import './App.css';
+// import { Outlet } from 'react-router-dom';
+// import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+// import PlayerInfoCard from './components/PlayerInfoCard';
+// import Header from './components/Header';
+// import TeamOrPass from './components/TeamOrPass';
+
+// import FriendList from './components/FriendList';
+
+// const client = new ApolloClient({
+//   uri: '/graphql',
+//   cache: new InMemoryCache(),
+// });
+
+// function App() {
+//   return (
+//     <ApolloProvider client={client}>
+//       <PlayerInfoCard user={user}/>
+//       <div className="flex-column justify-flex-start min-100-vh">
+//         <Header />
+//         <TeamOrPass />
+//         <FriendList />
+//         <div className="flex-column justify-center align-center min-100-vh bg-primary">
+//           <Outlet />
+//         </div>
+//       </div>
+//     </ApolloProvider>
+//   );
+// }
+
+// export default App;
+
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import PlayerInfoCard from './components/PlayerInfoCard';
+// import PlayerInfoCard from './components/PlayerInfoCard';
 import Header from './components/Header';
 import TeamOrPass from './components/TeamOrPass';
 
 import FriendList from './components/FriendList';
+// import TemporaryDrawer from './components/FriendList';
 
+// Proxy error shenanigans here
 const client = new ApolloClient({
   uri: '/graphql',
   cache: new InMemoryCache(),
@@ -15,7 +49,7 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <PlayerInfoCard user={user}/>
+      {/* <PlayerInfoCard user={user}/> */}
       <div className="flex-column justify-flex-start min-100-vh">
         <Header />
         <TeamOrPass />
