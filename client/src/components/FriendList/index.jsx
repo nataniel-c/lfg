2041ee4f229c51562b8cbb486cc1ed76628219c7
Nightfeller <family2earls@gmail.com>
2024-01-25 18:58:18 -0600

    modified friendList to show static names (while working around multiple error).

--- client/src/components/FriendList/index.jsx
+++ client/src/components/FriendList/index.jsx
@@ -30,25 +30,9 @@ export default function FriendList() {
         onKeyDown={toggleDrawer(anchor, false)}
         >
             <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                {['Obamamama', 'HubertN', 'john_doe', 'jane_smith'].map((text, index) => (
                     <ListItem key={text} disablePadding>
                         <ListItemButton>
-                            <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                            </ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
-            <Divider />
-            <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                            </ListItemIcon>
                             <ListItemText primary={text} />
                         </ListItemButton>
                     </ListItem>
